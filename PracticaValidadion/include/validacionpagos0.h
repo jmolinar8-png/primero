b58dba0977d35c2a8e24b3000d87b29fd60bb640
Josue-Molina <jmolinar8@miumg.edu.gt>
2026-04-22 19:32:01 -0600

    Biblioteca creada

---
+++ PracticaValidadion/include/validacionpagos0.h
@@ -0,0 +1,16 @@
+#ifndef VALIDACIONPAGOS0_H
+#define VALIDACIONPAGOS0_H
+
+
+class validacionpagos0
+{
+    public:
+        validacionpagos0();
+        virtual ~validacionpagos0();
+
+    protected:
+
+    private:
+};
+
+#endif // VALIDACIONPAGOS0_H
---
+++ PracticaValidadion/src/validacionpagos0.cpp
@@ -0,0 +1,11 @@
+#include "validacionpagos0.h"
+
+validacionpagos0::validacionpagos0()
+{
+    //ctor
+}
+
+validacionpagos0::~validacionpagos0()
+{
+    //dtor
+}
